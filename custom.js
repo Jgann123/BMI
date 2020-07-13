94f738e07d2972636acdad287e5705b2a5fbc6da
Jeremy Gann <56943728+Jgann123@users.noreply.github.com>
2020-07-12 20:30:58 -0400

    d

--- custom.js
+++ custom.js
@@ -1,32 +1,41 @@
-function bmi(){
-   //Get feet and inches value
-    let feet = document.querySelector(".feet").Value;
-    let inch = document.querySelector(".inch").Value;
-    //merge feet and inches value in oned
+function bmi() {
+    //Get feet and inches Value
+    let feet = document.querySelector(".feet").value;
+    let inch = document.querySelector(".inch").value;
+    // Merge feet and inches Value in one
     let metre = feet + "." + inch;
-    //feet to metre conversion and get a height in meters
+    //feet to metre conversion and get The height in metre
     let height = metre / 3.2808;
-    //get weight value
-    let weight = document.querySelector(".weight").Value;
-    //BMI calculation
+    // Get Weight Value
+    let weight = document.querySelector(".weight").value;
+    //BMI Calculation
     let bmi = weight / Math.pow(height, 2);
-    //BMI showing in p tag with condition
-    if(bmi<18.5){
-        document.querySelector("p").innerHTML = "Your Body Mass Index (BMI) is-: "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+"Index Value is Under Weight";
-
+    //BMI Showing in P Tag With condition
+    if (bmi < 18.5) {
+        document.querySelector("p").innerHTML =
+            "Your Body Mass Index (BMI) is-: " +
+            (Math.round(bmi * 100) / 100).toFixed(2) +
+            "<br>" +
+            " Index Value is in Under Weight";
+    } else if (bmi > 18.5 && bmi <= 24.9) {
+        document.querySelector("p").innerHTML =
+            "Your Body Mass Index (BMI) is-: " +
+            (Math.round(bmi * 100) / 100).toFixed(2) +
+            "<br>" +
+            " Index Value is in Normal";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        document.querySelector("p").innerHTML =
+            "Your Body Mass Index (BMI) is-: " +
+            (Math.round(bmi * 100) / 100).toFixed(2) +
+            "<br>" +
+            " Index Value is in Over Weight";
+    } else if (bmi > 30) {
+        document.querySelector("p").innerHTML =
+            "Your Body Mass Index (BMI) is-: " +
+            (Math.round(bmi * 100) / 100).toFixed(2) +
+            "<br>" +
+            " Index Value is in Obesity";
     }
-
-    else if(bmi >= 25 && bmi <= 29.9) {
-        document.querySelector("p").innerHTML = "Your Body Mass Index (BMI) is-: "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+"Index Value is Over Weight";
-        
-    }
-    else if(bmi > 30) {
-        document.querySelector("p").innerHTML = "Your Body Mass Index (BMI) is-: "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+"Index Value is in Obesity";
-
-    }
-
 }
-
-//Click Event Fire On calculator Button
-
+//Click Event on Calculate Button
 document.querySelector(".btn").addEventListener("click", bmi);
